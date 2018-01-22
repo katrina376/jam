b5b3ac1750893f79395540c598d82ed031110943
Katrina Chan <katrina.hyc@gmail.com>
2018-01-22 18:16:19 +0800

    Add comments to Comment module

--- core/models/comment.py
+++ core/models/comment.py
@@ -8,6 +8,25 @@ class CommentManager(models.Manager):
     """Manager of Comment model."""
 
     def create(self, *args, **kwargs):
+        """Overrided create method with validations.
+
+        Check if either `talk` or `comment` is null. Only one of them can be
+        null. If `talk` is not null, which means the comment is directly to a
+        talk, it makes the created instance a root comment. On the other hand,
+        if `comment` is not null, which means it is a reply to another comment,
+        it makes the created instance a leaf comment.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Instance of the created vote.
+
+        Raises:
+            ValidationError: If `user` has already voted on `comment`.
+        """
+
         talk = kwargs.get('talk', None)
         comment = kwargs.get('comment', None)
 
@@ -20,7 +39,30 @@ class CommentManager(models.Manager):
 
 
 class Comment(models.Model):
-    """Comment to a talk or another comment."""
+    """Comment to a talk or another comment.
+
+    Use `Comment.object.create(*args, **kwargs)` to create an instance.
+    Arguments are listed below.
+
+    Args:
+        content (str):
+        user (:obj:`User`):
+        talk (:obj:`User`, optional): Talk of the comment. Should be null if
+            `comment` is not null.
+        comment (:obj:`Comment`, optional): Target comment of the comment.
+            Should be null if `talk` is not null.
+
+    Attributes:
+        content (str):
+        create_time (datetime): Create time of the comment.
+        last_update (datetime): Last update time of the comment.
+        user (:obj:`User`):
+        talk (:obj:`User`): Talk of the comment. Should be null if `comment` is
+            not null.
+        comment (:obj:`Comment`): Target comment of the comment. Should be null
+            if `talk` is not null.
+        objects (:obj:`CommentManager`): Manager of Comment model.
+    """
 
     content = models.TextField()
     create_time = models.DateTimeField(auto_now=True)
